ec6a8581a8ebe562a220ef700820774820582313
Amanda Koster <amandakoster@gmail.com>
2017-06-09 16:24:59 -0700

    added projectView projectRender and revised Index with hbars template

---
+++ starter-code/public/data/projectData.js
@@ -0,0 +1,79 @@
+let projectData = [
+{
+title: "SalaamGarage",
+role: "Founder, Executive Director, Product Manager, UX",
+category: "UX",
+author: "Amanda Koster",
+projectUrl: "http://www.salaamgarage.com",
+overview: "some text",
+challenge: "<p> In 2004 my social documentary projects with nonprofits became well known. Between 2004 and 2006 I was approached by over 170 content producers who wanted to work with me on projects about nonprofits. I had also worked with and built relationships with nonprofits worldwide who shared one common problem, they did not have the capacity to tell their own story.t</p>",
+approach: "I researched over 30 NGOs and nonprofits to understand their messaging pains and content needs. I gained a deeper understanding of their stories and persuaded a few organizations that their stakeholders personal stories were the true message of their organization and that ultimately, though they had donors and board members, their actual stakeholders were the people they served. From there, I researched several voluntourism companies to understand their business model and how I to leverage that expertise for the concept of SalaamGarage. I also built noncompete business collaborations in order to outsource parts of SalaamGarage operations that didn't need reinventing such as the booking process, building tourist itineraries, and in-country resources.To market SalaamGarage, I leveraged my public speaking and book tours. I also followed up on over 100 email requests from content producers to ‘pick my brain’ and ‘be my photo assistant.’ I defined this audience as my target customer and offered them a chance to test SalaamGarage’s discounted, pilot trip to India.",
+
+process: "Building trips and storytelling itineraries was a lot of fun because I am passionate about this space, UX, research, and product design. However, it took a lot of effort to convert interested participants because the margin was low and my target customer had a hard time reaching the cost of a trip. In an effort to scale, I contracted skilled content producers to lead SalaamGarage trips. However, because I was the face of the company, the target customer wanted specifically to travel with me and that made scaling more challenging.A few investors reached out to me and though their ideas to grow the business were measurable and valid, SalaamGarage was near and dear to my heart and I didn’t want to commercialize the company the way they suggested. I suffered from Founder's Dilemma.",
+responsibilities:"some text",
+
+results: "I hired, trained, and managed trip leaders to lead international trips: sourced, screened, interviewed prospective candidates and matched leaders with complementary groups. I managed all media relations as spokesperson for the organization both in the U.S. and abroad, personally led and produced global projects in Ethiopia, India, Guatemala, Morocco, South Africa, Kenya, and Vietnam, and effectively managed across cultures to build media teams in USA, Kenya and South Africa.I formed a partnership with Blurb Books and published an India, Ethiopia, and Guatemala books which were featured on the Blurb “Blurb for Good“ website ‘editors pick.' We launched 5 exhibitions resulted in matching NGOs with Microsoft’s Employee Giving Program and raising thousands for the nonprofits.SalaamGarage trips were featured in The Huffington Post, Social Documentary, First Post, aCurator, Cluster Munition Coalition, Seattle Post Intelligencer, and many other media outlets and blogs. I gave an Ignite talk that instantly raised $2100 for 7 fistula surgeries, and SalaamGarage participants raised thousands more dollars for participating non-profits which funded school fees, livestock, and various other local needs.I defined three KPIs. First, repeat participants, travellers and non-profits. Second, the non-profit benefited from our projects. Meaning, we set goals and met them such as funding a child's school fees, purchasing a goat, building an exhibition, etc. Third, SalaamGarage was to profit in such a way that it would scale. The first two KPIs were met. Where we failed was the ability to scale."
+},
+
+{
+title: "Amazon 'KINDLE IN MOTION'",
+role: "SR Content Producer",
+category: "UX",
+author: "Amanda Koster",
+projectUrl: "https://www.amazon.com/Kindle-in-Motion/b?ie=UTF8&node=14530743011",
+overveiw: "While at Amazon Publishing, I developed and designed the creative vision and content strategy, managed teams of creatives, produced, art directed, QA'd, and launched the inaugural Kindle In Motion product. Pulling from my work history as an Executive Producer, Creative Director, and Product Manager, I approached Kindle In Motion similar to the short-form video, cinematic process, while retaining several distinct differences.",
+responsibilities: "Led the full lifecycle of product development; including ideation, strategy, design, development, implementation, and testing.Developed ideas through creating scenarios, sketches, storyboards, wireframes, motion studies, mock—ups, and prototypes.
+Championed concepts and ideas to diverse groups of developers, editorial leaders, and executives in ad-hoc and formal presentations.Drafted, edited, and presented content proposals that supported and advanced Amazon Publishing. Collaborated with editors, developers, designers, product managers, internal business partners, and other subject matter experts.
+Collected and analyzed data to support further iteration and decision-making.
+Authored extensive RFPs, SOPs, Creative Briefs, author goals, templates, and systems to scale the program.Worked with legal team to review contractual terms and conditions related to new and ongoing agreements.Delivered feedback and coached artists to improve material.
+Recruited and developed junior creatives and Identified, on-boarded, and built a nationwide roster of vendors.Led resources allocation for projects and team.
+Generated, proposed, and executed compelling storytelling solutions ideas based on specific content needs.",
+results: "<p>My Kindle In Motion titles received a minimum of 4.5 out of 5-star review (out of 240, 615, 279, and 405 reviews), and one received Best Seller within 30 days of launch.
+Led a brainstorming/think tank that generated seven pre-patent applications.Built framework that reduced production costs by 55% and enabled program to scale 10x the following year.</p>",
+},
+{
+title: "AIDS Is Knocking",
+role: "DP, Creative Director, and Executive Producer",
+category: "UX",
+author: "Amanda Koster",
+projectUrl: "https://spark.adobe.com/page/knZ9pqyHpcxuO/",
+overview: "“AIDS Is Knocking” - Project in collaboration with Rabuor Village Project. Produced a visual experience to impact perception about victims living with AIDS. This small American/Kenyan NGO. needed collateral to visually tell the story of their efforts in the region.",
+challenge: "<p>I became frustrated and fearingly desensitized with the statistics around the Millions of AIDS orphans in Sub Saharan Africa. So, I strategized a project to document and give a face to these statistics. I collaborated with Rabuor Village Project while living and working in Takaungu, Kenya on another project. Rabuor, Kenya (near Lake Victoria) is a region greatly affected by HIV/Aids and extreme poverty.</p>",
+results:"Culled a massive body of documentary-style collateral telling the story of how one village was coping with the devastation of the HIV/AIDs epidemic and the crippling poverty that accompanies it. Focused heavily on ethical journalism and pre-production with NGO to identify willing participants in country. Identified and enlisted a small, local team to assist the production video and multimedia content. Awarded second place in international LUCIE awards, several national grants, and raised more than $10,000 in funding from public and private sources. Traveling exhibitions, newspaper articles, TV interviews and public speaking at high schools and universities.. Funded by self and private donors, Rabuor Village Project, Kodak, Glazers Camera, Ivey imaging.Built and amazing team with Loyce Mbewa-Ong’udi (SME and Seattle production support), Kennedy Ong’udi (Kenya production support), Sara Teitelman, MPH (production support), Letitia Reason, PhD (Medical Anthropologist).The project won The International IPA Lucie Awards: 2nd place ‘people’ (All 8 entries), Society of Professional Journalism (national): 1st place photo essay, It Takes a Village.",
+},
+
+{
+title: "This Is Beautiful",
+role: "Creator, Creative Director and Executive Producer",
+category: "UX",
+author: "Amanda Koster",
+projectUrl: "https://spark.adobe.com/page/bwc88yfzPgvpU/",
+overviww: "Created by Amanda Koster (with a lot of support!), This Is Beautiful is an interactive media project showcasing the innate beauty of all women, regardless of age, race, ability, and size. This Is Beautiful spurred dialogue about women’s bodies and the lack of realistic images of women in media, a critical factor in the rise of eating disorders and distorted body image.",
+challenge: "<p>Throughout my professional photography career, most women were worried whether they would ‘look fat’ in their photograph. I attributed this to the bombardment of images depicting irregularly slim women. Therefore, I wanted to create a body of work and a movement that equally bombarded the viewer with normal looking women and their ideas around body image.</p>",
+approach: "I interviewed over 30 women about their body image and where they thought it stemmed from. I also researched eating disorder data that spanned various ages, ethnicities, abilities, and cultures. I concluded that photos would not thoroughly illustrate this topic and I wanted to include video interviews and text in the body of work.From there I authored a call-out and questionnaire to recruit participants. Pulling from my photography background I authored fully transparent model releases and questionnaire. I was awarded over $20k in artistic grants, in-kind donations, and procured over 15 sponsorships. I also recruited and managed a team of seven throughout the project.",
+results: "<p>My team secured over $15,000 of in-kind donations in USA and Canada, including Kodak, Glazers, and PhotoLab. As Creative Director and Executive Producer, I led a production team of seven camera technicians, project managers, editors, and site scouts. We recruited approximately 100 volunteer participants from USA and Canada for photos shoot by leveraging social and traditional media outlets and local relevant non-profits and organizations. We produced five interactive exhibitions in USA and Canada. I was invited to speak at numerous meetings and media events, including FOX News, and over 10 international universities.</p>",
+body: "<p>a whole bunch of text</p>"
+},
+
+{
+title: "DisastARcons",
+role: "UX, Web Development, and Project Manager",
+author: "Amanda Koster",
+projectUrl: "http://www.salaamgarage.com",
+overview: "An 8.8 earthquake happens causing a devastating tsunami in Seattle. 1st responders have performed search and rescue. You are a member of FEMA (Federal Emergency Management Agency), responsible for the coordination and response to a disaster that has occurred in the United States and that overwhelms the resources of local and state authorities.",
+challenge: "<p>A Government Accountability Office (GAO) 2015 audit report of FEMA found response capability gaps through national-level exercises and real-world incidents. The audit also found the status of FEMAs actions to address these gaps was not collected by or reported to the Department of Homeland Security or Federal Emergency Management Agency (FEMA). --Anthony Kimery, Editor-in-Chief, Homeland Security Today.</p>",
+process: "DisastARcons uses the Microsoft HoloLens for damage assessment by visually inspecting and marking areas that need attention or that are health/safety risks. DisastARcons increases efficiency in capturing and sharing accurate data AND measures the time between identification and resolution. Why Hololens? Always in front of you. The HoloLens utilizes the user's entire field of view vs. most devices, such as a cell phone (Google Cardboard) that uses a limited rectangle of view and dependent in the users holding the device. Example use case: second shift, all data will always be accessible and compiled by all user. Hands free. The HoloLen has the highest fidelity, it can do 3D, 360° (4π steradian) constitution of entire surroundings. The HoloLen is always in front of you and utilizes the user's entire field of view vs. most devices, such as a cell phone (Google Cardboard) that uses a limited rectangle of view and dependent in the users holding the device. Example use case\ second shift, all data will always be accessible and compiled by all user. Hands free.",
+results: "<p>Winner, Best Humanitarian Mixed Reality Tool, Seattle VR Hackathon 2016</p>",
+},
+
+{
+title: "DirtyGirlz",
+category: "UX",
+author: "Amanda Koster",
+projectUrl: "http://www.dirtygirlz.us/",
+challenge: "<p>a whole bunch of text</p>",
+results: "<p>a whole bunch of text</p>",
+body: "<p>a whole bunch of text</p>"
+}
+
+]
--- starter-code/public/scripts/views/projectView.js
+++ starter-code/public/scripts/views/projectView.js
@@ -1,1 +1,6 @@
+'use-strict';
 
+$('#team-button').click(function() {
+  $('#about, main').hide();
+  $('#projects').show();
+});
